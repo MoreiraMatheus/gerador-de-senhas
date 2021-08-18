8d1de395948a8f81a4714fe3eefb58db5f626bf9
Matheus M <matheus.m.santana87@gmail.com>
2021-08-18 17:21:35 -0300

    Tratamento de erros atualizado

--- senhas.py
+++ senhas.py
@@ -1,33 +1,33 @@
+# imports
 from random import choice
 import PySimpleGUI as sg
 
+#variáveis
 lnum = "1234567890" #lista de numeros
 lsimb = "!@#$%&." #lista de simbolos
 llet = "abcdefghijklmnopqrstuvwxyz" #alfabeto
 
-#layout
-sg.theme('dark')
+#layout janela
+sg.theme('dark') #tema
 layout = [
     [sg.Text('Caracteres:'), sg.Input(key='caracteres', size=(6, 0))],
     [sg.Text('Sua senha terá:')],
-    [sg.Checkbox('Numeros', key='num'), sg.Checkbox('letras', key='letras'), sg.Checkbox('simbolos', key='simb')],
-    [sg.Button('gerar senha', key='botao')],
+    [sg.Checkbox('Numeros', key='num'), sg.Checkbox('letras', key='letras'),
+     sg.Checkbox('simbolos', key='simb')],
+    [sg.Button('Gerar senha', key='botao')],
     [sg.Output(size=(40, 7))]
 ]
 
-#janela
+#janela do programa
 janela = sg.Window('Gerador de senhas', layout)
-
 while True:
     eventos, valores = janela.read()
-    if eventos == sg.WINDOW_CLOSED:
+    if eventos == sg.WINDOW_CLOSED: #caso o usuário feche a janela
         break
-    if eventos == 'botao':
+    if eventos == 'botao': #quando o usurário aperta no botão "gerar senha"
         try:
-            senha = "" #o resultado que será devolvido
+            senha = "" #o resultado que será devolvido ficará aqui
             tam = int(valores['caracteres'])
-            if tam == 0:
-                print('Para gerar sua senha é necessário escolher o tamanho dela.')
             listas = [] #conteudo para gerar a senha
             if valores['num'] == True:
                 listas += lnum
@@ -35,16 +35,16 @@ while True:
                 listas += lsimb
             if valores['letras'] == True:
                 listas += llet
-            if valores['num'] == False and valores['simb'] == False and valores['letras'] == False:
+            try:
+                for c in range(0, tam):
+                    letra = choice(listas)
+                    senha += letra
+                print(f"A senha que geramos foi essa: {senha}") #finalmete temos nossa senha pronta
+            except IndexError:
                 print('Marque pelo menos uma checkbox')
-            else:
-                try:
-                    for c in range(0, tam):
-                        letra = choice(listas)
-                        senha += letra
-                    print(f"A senha que geramos foi essa: {senha}") #finalmete temos nossa senha pronta
-                except:
-                    print('Houve um erro na hora de gerarmos sua senha.')
-        except:
-            print('Ocorreu um erro inesperado.')
-# usar o tratamento de excessões para gerar mensagens diferentes para o usuário
+            except Exception as erro:
+                print(f'Houve um erro na hora de gerarmos sua senha.\nErro: {erro.__class__}')
+        except ValueError:
+            print('Escolha o tamanho da sua senha.')
+        except Exception as erro:
+            print(f'Ocorreu um erro inesperado.\nErro: {erro.__class__}')
